443ebe2b88fe5325febae3a8b97d00c7184440ab
johnjohnata123 <johnata.silva@estat.com.br>
2023-07-30 19:34:49 -0300

    modelo inicial

--- rdocs/johnata.R
+++ rdocs/johnata.R
@@ -1,5 +1,7 @@
 source("rdocs/source/packages.R")
-
+pacman::p_load(effects,car,erer,VGAM, caret, 
+               janitor,ltm,FSA,rcompanion,
+               PerformanceAnalytics, MASS,survey)
 # ---------------------------------------------------------------------------- #
 
 #        ______   _____  ________      ________ 
@@ -22,3 +24,25 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+plano$variables <- plano$variables %>%
+  mutate(b06_cor = case_when(
+    b06_cor == 'Branca' ~ 'nao negro',
+    b06_cor == 'Indígena ' ~ 'nao negro',
+    b06_cor == 'Amarela (origem japonesa, chinesa, coreana etc.)' ~ 'nao negro',
+    b06_cor == 'Preta' ~ 'negro',
+    b06_cor == 'Parda (mulata, cabocla, cafuza, mameluca ou mestiça)' ~ 'negro',
+  ))
+plano$variables <- plano$variables %>%
+  mutate(d13_febre_vacina = na_if(d13_febre_vacina, 'Não sabe'))
+
+mod1 <- svyglm(factor(CHIK_PRNT20) ~ factor(Sexo) + b05_idade + d13_febre_vacina + c01_tipo
+       +f11_c_g_valor + factor(b06_cor),family = binomial,design = plano)
+summary(mod1)
+
+
+
+
+
+
+
